19443f1f5bd21d901a7c76b8a1b56b15657b2e75
oscarcalle <oscarcalle0@gmail.com>
2024-10-07 06:43:43 -0500

    Environment variables are created and removed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
+//Environment variables are called
+using DotNetEnv;
+
+Env.Load();
+
+var DB_HOST = Environment.GetEnvironmentVariable("DB_HOST");
+var DB_NAME = Environment.GetEnvironmentVariable("DB_NAME");
+var DB_PORT = Environment.GetEnvironmentVariable("DB_PORT");
+var DB_USER = Environment.GetEnvironmentVariable("DB_USER");
+var DB_PASSWORD = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+
+var stringConnection = $"server={DB_HOST};port={DB_PORT};database={DB_NAME};uid={DB_USER};password={DB_PASSWORD}";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
